cbdbc35fe189b4afa0674b9d758dfb7df6f89b52
Camilascalco <128312038+Camilascalco@users.noreply.github.com>
2024-07-02 10:38:00 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
-import csv
-import random
-pessoas = []
-with open('maria.csv', 'r') as sei_la:
-  leitor = csv.reader(sei_la)
+import csv 
+import random 
+pessoas=[]
+with open('camila.csv','r') as sei_la:
+  ler=csv.reader(sei_la)
+  for linha in ler:
+    pessoas.append(linha) 
 
-  for linha in leitor:
-    pessoas.append(linha)
-random.shuffle(pessoas)
+def ler_aventura(p1):
+  for linlin in p1:
+    print(linlin)
 
-for linlin in pessoas:
-  print(linlin)
-  
+def esperar_continuar():
+  continuar = input("digite c para continuar")
+  while continuar != "c" and continuar != "C":
+    continuar = input("digite c para continuar")
+
+def mostra_aventura(p1, indice):
+  if indice >= 0 and indice < len(p1):
+    print(p1[indice])
 
 
+def remover_aventura(p1, indice):
+  total = len(p1)-1
+  if indice >= 0 and indice < total:
+    p1.pop(indice)
+    print("Aventura removida com sucesso!")
+    for i in p1:
+      print(i)
+  return p1
 
+ler_aventura(pessoas)
+esperar_continuar()
+indice_aventura = int(input("Digite o indice da aventura: "))
+mostra_aventura(pessoas, indice_aventura)
+novo = remover_aventura(pessoas, indice_aventura)
 
-
-def ler_aventura(vetor, indice):
-  if indice < 0 or indice >= len(vetor):
-      print("Índice fora dos limites do vetor.")
-      return None
-  else:
-      return vetor[indicO guarda virou zumbie]
-
-# Exemplo de uso:
-aventuras = [
-  "Chegou ao laboratório de ciências.",
-  "As luzes se apagam.",
-  "O guarda virou zumbi."
-]
-
-indice_aventura = 1  # Índice da aventura que queremos ler
-
-aventura_escolhida = ler_aventura(aventuras, indice_aventura)
-
-if aventura_escolhida:
-  print("Aventura escolhida:")
-  print(aventura_escolhida)
 
